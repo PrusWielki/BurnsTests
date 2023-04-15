ea10759bd628cbb775bd30dd1e0b45474948ef67
Patryk <pruswielki@gmail.com>
2023-04-15 14:02:12 +0200

    add homepage design

--- src/components/home/home_container/home_container.svelte
+++ src/components/home/home_container/home_container.svelte
@@ -2,9 +2,35 @@
 </script>
 
 <div id="home-grid" class="grid h-screen w-screen grid-cols-3">
-	<div id="option1-wrapper h-full w-full bg-[url('/background/animals.jpg')]">
-		<div id="option1" class=" font-sans text-2xl font-bold md:text-6xl">Option 1</div>
+	<div
+		id="option1-wrapper"
+		class="group flex h-full w-full items-center justify-center bg-opacity-80 bg-[url('/background/mountains.jpg')] bg-cover bg-center after:absolute after:left-0 after:top-0 after:h-full after:w-1/3 after:bg-black after:bg-opacity-30 after:transition-all after:duration-500 after:content-[''] hover:after:opacity-10"
+	>
+		<div
+			id="option1"
+			class="z-10 font-sans text-2xl font-bold text-white transition duration-300 group-hover:-translate-y-1 md:text-6xl"
+		>
+			Option 1
+		</div>
 	</div>
-	<div>Option 2</div>
-	<div>Option 2</div>
+	<div
+		id="option2-wrapper"
+		class="group flex h-full w-full items-center justify-center bg-opacity-80 bg-[url('/background/animals.jpg')] bg-cover bg-center after:absolute after:left-1/3 after:top-0 after:h-full after:w-1/3 after:bg-black after:bg-opacity-30 after:transition-all after:duration-500 after:content-[''] hover:after:opacity-10"
+	>
+		<div
+			class="z-10 font-sans text-2xl font-bold text-white transition duration-300 group-hover:-translate-y-1 md:text-6xl"
+		>
+			Option 2
+		</div>
+	</div>
+	<div
+		id="option3-wrapper"
+		class="after:left- group flex h-full w-full items-center justify-center bg-opacity-80 bg-[url('/background/trees.jpg')] bg-cover bg-center after:absolute after:top-0 after:h-full after:w-1/3 after:bg-black after:bg-opacity-30 after:transition-all after:duration-500 after:content-[''] hover:after:opacity-10"
+	>
+		<div
+			class="z-10 font-sans text-2xl font-bold text-white transition duration-300 group-hover:-translate-y-1 md:text-6xl"
+		>
+			Option 2
+		</div>
+	</div>
 </div>
