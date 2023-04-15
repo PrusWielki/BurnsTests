0155bec8376b55d88d9adf1de8230eebcec69792
Patryk <pruswielki@gmail.com>
2023-04-15 14:14:13 +0200

    cursor pointer

--- src/components/home/home_container/home_container.svelte
+++ src/components/home/home_container/home_container.svelte
@@ -4,7 +4,7 @@
 <div id="home-grid" class="grid h-screen w-screen grid-rows-3 sm:grid-cols-3">
 	<div
 		id="option1-wrapper"
-		class="group flex h-full w-full items-center justify-center bg-opacity-80 bg-[url('/background/mountains.jpg')] bg-cover bg-center after:absolute after:left-0 after:top-0 after:h-1/3 after:w-full after:bg-black after:bg-opacity-30 after:transition-all after:duration-500 after:content-[''] hover:after:opacity-10 sm:h-screen after:sm:h-full after:sm:w-1/3"
+		class="group flex h-full w-full cursor-pointer items-center justify-center bg-opacity-80 bg-[url('/background/mountains.jpg')] bg-cover bg-center after:absolute after:left-0 after:top-0 after:h-1/3 after:w-full after:bg-black after:bg-opacity-30 after:transition-all after:duration-500 after:content-[''] hover:after:opacity-10 sm:h-screen after:sm:h-full after:sm:w-1/3"
 	>
 		<div
 			id="option1"
@@ -15,7 +15,7 @@
 	</div>
 	<div
 		id="option2-wrapper"
-		class="group flex h-full w-full items-center justify-center bg-opacity-80 bg-[url('/background/animals.jpg')] bg-cover bg-center after:absolute after:left-0 after:top-1/3 after:h-1/3 after:w-full after:bg-black after:bg-opacity-30 after:transition-all after:duration-500 after:content-[''] hover:after:opacity-10 sm:h-screen after:sm:left-1/3 after:sm:top-0 after:sm:h-full after:sm:w-1/3"
+		class="group flex h-full w-full cursor-pointer items-center justify-center bg-opacity-80 bg-[url('/background/trees.jpg')] bg-cover bg-center after:absolute after:left-0 after:top-1/3 after:h-1/3 after:w-full after:bg-black after:bg-opacity-30 after:transition-all after:duration-500 after:content-[''] hover:after:opacity-10 sm:h-screen after:sm:left-1/3 after:sm:top-0 after:sm:h-full after:sm:w-1/3"
 	>
 		<div
 			class="z-10 font-sans text-2xl font-bold text-white transition duration-300 group-hover:-translate-y-1 md:text-6xl"
@@ -25,7 +25,7 @@
 	</div>
 	<div
 		id="option3-wrapper"
-		class="group flex h-full w-full items-center justify-center bg-opacity-80 bg-[url('/background/trees.jpg')] bg-cover bg-center after:absolute after:left-0 after:top-2/3 after:h-1/3 after:w-full after:bg-black after:bg-opacity-30 after:transition-all after:duration-500 after:content-[''] hover:after:opacity-10 sm:h-screen after:sm:left-2/3 after:sm:top-0 after:sm:h-full after:sm:w-1/3"
+		class="group flex h-full w-full cursor-pointer items-center justify-center bg-opacity-80 bg-[url('/background/animals.jpg')] bg-cover bg-center after:absolute after:left-0 after:top-2/3 after:h-1/3 after:w-full after:bg-black after:bg-opacity-30 after:transition-all after:duration-500 after:content-[''] hover:after:opacity-10 sm:h-screen after:sm:left-2/3 after:sm:top-0 after:sm:h-full after:sm:w-1/3"
 	>
 		<div
 			class="z-10 font-sans text-2xl font-bold text-white transition duration-300 group-hover:-translate-y-1 md:text-6xl"
