b50e8d5698d2eed8ca53707da8992abd04517bd8
Patryk <pruswielki@gmail.com>
2023-04-16 23:56:43 +0200

    refactor option component

--- src/components/home/home_container/home_container.svelte
+++ src/components/home/home_container/home_container.svelte
@@ -1,64 +1,11 @@
 <script lang="ts">
 	import { TEST_NAMES } from '../../../cms/home/home';
+	import Option from '../option/option.svelte';
 	let active: Array<boolean> = [false, false, false];
 </script>
 
 <div id="home-grid" class="grid h-screen w-screen grid-rows-3 sm:grid-cols-3">
-	<div
-		id="option1-wrapper"
-		class={`${
-			active[0] ? '-translate-y-full transition duration-700' : ''
-		} group flex h-full w-full cursor-pointer   items-center justify-center bg-opacity-80 bg-[url('/background/mountains.jpg')] bg-cover bg-center after:absolute after:left-0 after:top-0 after:h-1/3 after:w-full after:bg-black after:bg-opacity-30 after:transition-all after:duration-500 after:content-[''] hover:after:opacity-10 sm:h-screen after:sm:h-full after:sm:w-1/3`}
-		on:click={() => {
-			active[0] = true;
-		}}
-		on:keypress={() => {
-			active[0] = true;
-		}}
-	>
-		<div
-			id="option1"
-			class="z-10 font-sans text-2xl font-bold text-white transition duration-300 group-hover:-translate-y-1 md:text-6xl"
-		>
-			{TEST_NAMES[0]}
-		</div>
-	</div>
-	<div
-		id="option2-wrapper"
-		class={`${
-			active[1] ? '-translate-y-full transition duration-700' : ''
-		} group flex h-full w-full  cursor-pointer items-center justify-center bg-opacity-80 bg-[url('/background/trees.jpg')] bg-cover bg-center after:absolute after:left-0 after:top-1/3 after:h-1/3 after:w-full after:bg-black after:bg-opacity-30 after:transition-all after:duration-500 after:content-[''] hover:after:opacity-10 sm:h-screen after:sm:left-1/3 after:sm:top-0 after:sm:h-full after:sm:w-1/3`}
-		on:click={() => {
-			active[1] = true;
-		}}
-		on:keypress={() => {
-			active[1] = true;
-		}}
-	>
-		<div
-			class="z-10 font-sans text-2xl font-bold text-white transition duration-300 group-hover:-translate-y-1 md:text-6xl"
-			id="option2"
-		>
-			{TEST_NAMES[1]}
-		</div>
-	</div>
-	<div
-		id="option3-wrapper"
-		class={`${
-			active[2] ? '-translate-y-full transition duration-700' : ''
-		} group flex h-full w-full cursor-pointer items-center justify-center bg-opacity-80 bg-[url('/background/animals.jpg')] bg-cover bg-center after:absolute after:left-0 after:top-2/3 after:h-1/3 after:w-full after:bg-black after:bg-opacity-30 after:transition-all after:duration-500 after:content-[''] hover:after:opacity-10 sm:h-screen after:sm:left-2/3 after:sm:top-0 after:sm:h-full after:sm:w-1/3`}
-		on:click={() => {
-			active[2] = true;
-		}}
-		on:keypress={() => {
-			active[2] = true;
-		}}
-	>
-		<div
-			class="z-10 font-sans text-2xl font-bold text-white transition duration-300 group-hover:-translate-y-1 md:text-6xl"
-			id="option3"
-		>
-			{TEST_NAMES[2]}
-		</div>
-	</div>
+	{#each TEST_NAMES as testName, index}
+		<Option bind:active={active[index]} {testName} />
+	{/each}
 </div>
