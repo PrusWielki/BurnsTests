e00f2bad3deb944a1a2b500a7c55c8c27b1b2098
Patryk <pruswielki@gmail.com>
2023-04-17 19:29:32 +0200

    add save button

--- src/components/home/home_container/home_container.svelte
+++ src/components/home/home_container/home_container.svelte
@@ -10,11 +10,11 @@
 	class="grid h-screen w-screen grid-rows-3 overflow-hidden sm:grid-cols-3 sm:grid-rows-none"
 >
 	{#each TEST_NAMES as testName, index}
-		<div class="grid grid-cols-1 grid-rows-1">
+		<div id="option-container" class="grid grid-cols-1 grid-rows-1">
 			{#if active[index] === false}
 				<Option bind:active={active[index]} {testName} backgroundName={BACKGROUND_NAMES[index]} />
 			{:else}
-				<TestComponent />
+				<TestComponent bind:active={active[index]} />
 			{/if}
 		</div>
 	{/each}
--- src/components/test/test_component/test_component.svelte
+++ src/components/test/test_component/test_component.svelte
@@ -6,15 +6,17 @@
 	export const minRange: number = 0;
 	export const maxRange: number = 4;
 	export const helpDescription: string = '';
+	export let active: boolean = false;
 </script>
 
 <div
+	id="test-container"
 	class="absolute z-10 flex h-full w-1/3 flex-col items-center justify-center"
 	transition:fade={{ delay: 300, duration: 500 }}
 >
 	<div>{title}</div>
-	{#each questions as question}
-		<div class="text-white">{question}</div>
-		<button class="btn">jk</button>
+	{#each questions as question, index}
+		<div id={`question-${index}`} class="text-white">{question}</div>
 	{/each}
+	<button id="save-button" class="btn" on:click={() => (active = !active)}>save</button>
 </div>
