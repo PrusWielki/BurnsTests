314f2b0b47b27fe1087372bb228558897292b353
Patryk <pruswielki@gmail.com>
2023-04-17 19:53:38 +0200

    progress towards test

--- src/components/test/test_component/test_component.svelte
+++ src/components/test/test_component/test_component.svelte
@@ -6,17 +6,35 @@
 	export const minRange: number = 0;
 	export const maxRange: number = 4;
 	export const helpDescription: string = '';
+	export let answerSet: Array<number> = [];
 	export let active: boolean = false;
 </script>
 
 <div
 	id="test-container"
-	class="absolute z-10 flex h-full w-1/3 flex-col items-center justify-center"
+	class="absolute z-10 flex h-full w-1/3 flex-col items-center justify-center gap-4"
 	transition:fade={{ delay: 300, duration: 500 }}
 >
-	<div>{title}</div>
+	<div class="text-4xl">{title}</div>
 	{#each questions as question, index}
-		<div id={`question-${index}`} class="text-white">{question}</div>
+		<div class="grid grid-cols-2">
+			<div id={`question-${index}`} class="text-white">{question}</div>
+			<div>
+				<input
+					type="range"
+					min={minRange}
+					max={maxRange}
+					value={answerSet[index]}
+					class="range range-sm"
+					step={1}
+				/>
+				<div class="flex w-full justify-between px-2 text-xs">
+					{#each Array(maxRange + 1) as _, i}
+						<span>{i}</span>
+					{/each}
+				</div>
+			</div>
+		</div>
 	{/each}
-	<button id="save-button" class="btn" on:click={() => (active = !active)}>save</button>
+	<button id="save-button" class="btn-outline" on:click={() => (active = !active)}>save</button>
 </div>
