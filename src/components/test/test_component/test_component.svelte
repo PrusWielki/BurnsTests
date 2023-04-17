c9c4073d7698c890640d811ede67414c54d773c5
Patryk <pruswielki@gmail.com>
2023-04-17 10:58:55 +0200

    save

--- src/components/test/test_component/test_component.svelte
+++ src/components/test/test_component/test_component.svelte
@@ -5,11 +5,12 @@
 	export const minRange: number = 0;
 	export const maxRange: number = 4;
 	export const helpDescription: string = '';
+	// Do something like that: animate the cards on some kind of onunload, dont render tests beforehand, add animation from below
 </script>
 
 <div class="flex h-full w-full max-w-xl flex-col items-center justify-center">
 	<div>{title}</div>
 	{#each questions as question}
-		<div>{question}</div>
+		<div class="text-white">{question}</div>
 	{/each}
 </div>
