ff2847b5436530d39d8a0160893ecf005496d27a
Patryk <pruswielki@gmail.com>
2023-04-17 10:31:10 +0200

    add correct images

--- src/components/home/home_container/home_container.svelte
+++ src/components/home/home_container/home_container.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import { TEST_NAMES } from '../../../cms/home/home';
+	import { TEST_NAMES, BACKGROUND_NAMES } from '../../../cms/home/home';
 	import Option from '../option/option.svelte';
 	let active: Array<boolean> = [false, false, false];
 </script>
 
 <div id="home-grid" class="grid h-screen w-screen grid-rows-3 sm:grid-cols-3">
 	{#each TEST_NAMES as testName, index}
-		<Option bind:active={active[index]} {testName} />
+		<Option bind:active={active[index]} {testName} backgroundName={BACKGROUND_NAMES[index]} />
 	{/each}
 </div>
--- src/components/home/option/option.svelte
+++ src/components/home/option/option.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	// import animals from '$lib/assets/background/animals.jpg';
+	export let backgroundName: string = '/background/animals.jpg';
 	export let testName: string = '';
 	export let active: boolean = false;
 </script>
 
 <div
 	id="option-wrapper"
+	style=" background-image: url({backgroundName})"
 	class={`${
 		active ? '-translate-y-full transition duration-700' : ''
-	} group flex h-full w-full cursor-pointer   items-center justify-center bg-opacity-80  bg-cover bg-center after:relative after:left-0 after:top-0 after:h-1/3 after:w-full after:bg-black after:bg-opacity-30 after:transition-all after:duration-500 after:content-[''] hover:after:opacity-10 sm:h-screen after:sm:h-full`}
+	} group flex h-full w-full cursor-pointer items-center  justify-center bg-opacity-80  bg-cover bg-center after:relative after:left-0 after:top-0 after:h-1/3 after:w-full after:bg-black after:bg-opacity-30 after:transition-all after:duration-500 after:content-[''] hover:after:opacity-10 sm:h-screen after:sm:h-full`}
 	on:click={() => {
 		active = true;
 	}}
