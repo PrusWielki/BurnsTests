e43897777a7ffc1a239e6049d957f9981f72f674
Patryk <pruswielki@gmail.com>
2023-04-18 13:17:56 +0200

    add scrollbar styling chrome

--- src/components/test/test_component/test_component.svelte
+++ src/components/test/test_component/test_component.svelte
@@ -15,29 +15,43 @@
 	transition:fade={{ delay: 300, duration: 500 }}
 >
 	<div class="text-center font-sans text-4xl font-bold capitalize text-zinc-400">{title}</div>
-	{#each questions as question, index}
-		<div class="grid grid-cols-2">
-			<div id={`question-${index}`} class="font-sans text-lg text-zinc-300">
-				{question}
-			</div>
-			<div>
-				<input
-					type="range"
-					min={minRange}
-					max={maxRange}
-					value={answerSet[index]}
-					class="range range-sm"
-					step={1}
-				/>
-				<div class="flex w-full justify-between px-2 text-xs">
-					{#each Array(maxRange + 1) as _, i}
-						<span>{i}</span>
-					{/each}
+	<div
+		id="question-set-container"
+		class="leanscroll flex max-h-96 flex-col gap-4 overflow-auto px-4"
+	>
+		{#each questions as question, index}
+			<div class="grid grid-cols-2">
+				<div id={`question-${index}`} class="font-sans text-lg text-zinc-300">asd</div>
+				<div>
+					<input
+						type="range"
+						min={minRange}
+						max={maxRange}
+						value={answerSet[index]}
+						class="range range-sm"
+						step={1}
+					/>
+					<div class="flex w-full justify-between px-2 text-xs">
+						{#each Array(maxRange + 1) as _, i}
+							<span>{i}</span>
+						{/each}
+					</div>
 				</div>
 			</div>
-		</div>
-	{/each}
+		{/each}
+	</div>
 	<button id="save-button" class="btn font-semibold" on:click={() => (active = !active)}
 		>save</button
 	>
 </div>
+
+<style>
+	.leanscroll::-webkit-scrollbar {
+		width: 4px;
+		margin-left: 10px;
+	}
+	.leanscroll::-webkit-scrollbar-thumb {
+		background: slateblue;
+		border-radius: 2px;
+	}
+</style>
