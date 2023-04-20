71ac228545d46c2b856a4b1f55bc5d8e091973dd
Patryk <pruswielki@gmail.com>
2023-04-20 21:27:31 +0200

    Add login/register

--- src/database/supabaseClient.js
+++ src/database/supabaseClient.js
@@ -1,6 +1,3 @@
 import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-	'https://fqwtfohqcstvlurpodxy.supabase.co',
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxd3Rmb2hxY3N0dmx1cnBvZHh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE4MjYxMzUsImV4cCI6MTk5NzQwMjEzNX0.aAHbvJUJcVs3rwsjdh3S6HGoRNZItuihQ4obaz9Cg2w'
-);
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,10 @@
+<div id="register-container">
+	<h1>Register</h1>
+	<form action="?/register" method="POST">
+		<label for="">Email</label>
+		<input type="text" name="email" />
+		<label for=""> Password </label>
+		<input type="password" name="password" />
+		<button class="btn-primary btn">Register</button>
+	</form>
+</div>
