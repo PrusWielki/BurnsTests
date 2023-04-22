b2f7162ca3b8955f425a120e2915e3b874e8a559
Patryk <pruswielki@gmail.com>
2023-04-22 16:12:37 +0200

    Login page styles

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,10 +1,21 @@
-<main>
-	<h1>Login</h1>
-	<form action="?/login" method="POST" class="auth-form">
-		<label for=""> Email </label>
-		<input type="text" name="email" />
-		<label for=""> Password </label>
-		<input type="password" name="password" />
-		<button type="submit" class="btn-primary btn">Login</button>
+<div id="login_main_container" class="flex h-screen w-screen flex-col items-center justify-center">
+	<form
+		action="?/login"
+		method="POST"
+		class="auth-form flex w-full flex-col items-center justify-center gap-4"
+	>
+		<div class="grid grid-cols-2">
+			<label for="" class="text-center text-slate-300 sm:text-2xl"> Email </label>
+			<input type="text" name="email" class="input-bordered input" placeholder="..." />
+		</div>
+		<div class="grid grid-cols-2">
+			<label for="" class="text-center text-slate-300 sm:text-2xl"> Password </label>
+			<input type="password" name="password" class="input-bordered input" placeholder="..." />
+		</div>
+		<button
+			type="submit"
+			class="btn text-slate-300 transition duration-75 hover:-translate-y-0.5 hover:shadow-lg sm:text-2xl"
+			>Login</button
+		>
 	</form>
-</main>
+</div>
