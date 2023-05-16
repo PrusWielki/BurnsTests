a5c61392d909250f55947ceacdabfb2f2045d81d
Patryk <pruswielki@gmail.com>
2023-05-16 17:29:36 +0200

    Creating a wrapping to implement navbar

---
+++ src/routes/(with_navbar)/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css';
+	import { fly } from 'svelte/transition';
+	import { goto } from '$app/navigation';
+</script>
+
+<div id="navbar" class="fixed top-0 -z-10 h-16 w-screen bg-transparent">
+	<button
+		in:fly={{ y: -screen.height / 2, duration: 1000 }}
+		class="btn fixed right-2 top-2 z-30 bg-transparent text-slate-300 transition duration-75 hover:-translate-y-0.5 hover:shadow-lg sm:right-10 sm:top-10 sm:text-2xl"
+		on:click={() => goto('/results')}>Results</button
+	>
+</div>
+<slot />
--- src/routes/(with_navbar)/tests/+page.svelte
+++ src/routes/(with_navbar)/tests/+page.svelte
@@ -50,11 +50,6 @@
 			>logout</button
 		>
 	</form>
-	<button
-		in:fly={{ y: -screen.height / 2, duration: 1000 }}
-		class="btn fixed right-2 top-2 z-30 bg-transparent text-slate-300 transition duration-75 hover:-translate-y-0.5 hover:shadow-lg sm:right-10 sm:top-10 sm:text-2xl"
-		on:click={() => goto('/results')}>Results</button
-	>
 	{#if !checkIfActive(active)}
 		{#each TEST_NAMES as testName, index}
 			<div id="option-container" class="grid grid-cols-1 grid-rows-1">
