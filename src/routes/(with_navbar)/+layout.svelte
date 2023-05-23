fa205a1f64421a64b8e165207b108a6fcd12e917
Patryk <pruswielki@gmail.com>
2023-05-23 15:57:21 +0200

    add but disable, for now, the back button

--- src/routes/(with_navbar)/+layout.svelte
+++ src/routes/(with_navbar)/+layout.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
 	import { fly } from 'svelte/transition';
 	import { goto } from '$app/navigation';
+	// import { browser } from '$app/environment';
 </script>
 
 <div id="navbar-container" class="fixed right-0 z-30 flex flex-row gap-1 px-1 py-1">
+	<!-- {#if browser && history.length > 2}
+		<a
+			id="navbar-tests-button"
+			in:fly={{ y: -screen.height / 2, duration: 1000 }}
+			class="btn z-30 bg-transparent text-slate-300 transition duration-75 hover:-translate-y-0.5 hover:shadow-lg sm:text-2xl"
+			on:click={() => history.back()}
+			on:keydown={() => history.back()}>Go Back</a
+		>
+	{/if} -->
 	<a
 		id="navbar-tests-button"
 		in:fly={{ y: -screen.height / 2, duration: 1000 }}
