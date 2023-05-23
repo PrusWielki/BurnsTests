946c2b1553a76deac32f29403f1387d81dc3bb1a
Patryk <78270281+PrusWielki@users.noreply.github.com>
2023-05-23 16:01:33 +0200

    Feature/navbar (#9)
    * navbar design, kindof
    
    * replace old buttons by new navbar
    
    * add but disable, for now, the back button
    
    * remove active modifier in navbar
    
    ---------
    
    Co-authored-by: Patryk <pruswielki@gmail.com>

--- src/routes/(with_navbar)/results/results/+page.svelte
+++ src/routes/(with_navbar)/results/results/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-	import { getPagination } from '../../../hooks/pagination';
+	import { getPagination } from '../../../../hooks/pagination';
 	import { fly } from 'svelte/transition';
-	import { goto } from '$app/navigation';
 	import type { PageData } from './$types';
 	export let data: PageData;
 	let { testData, supabase } = data;
@@ -33,19 +32,6 @@
 	class="flex h-screen w-screen flex-col items-center justify-center px-4 py-12"
 	in:fly={{ y: -screen.height / 2, duration: 1000 }}
 >
-	<form action="/logout" method="GET" class="fixed left-2 top-2 z-30 sm:left-10 sm:top-10">
-		<button
-			in:fly={{ y: -screen.height / 2, duration: 1000 }}
-			type="submit"
-			class="btn bg-transparent text-slate-300 transition duration-75 hover:-translate-y-0.5 hover:shadow-lg sm:text-2xl"
-			>logout</button
-		>
-	</form>
-	<button
-		in:fly={{ y: -screen.height / 2, duration: 1000 }}
-		class="btn fixed right-2 top-2 z-30 bg-transparent text-slate-300 transition duration-75 hover:-translate-y-0.5 hover:shadow-lg sm:right-10 sm:top-10 sm:text-2xl"
-		on:click={() => goto('/results')}>Back</button
-	>
 	{#if testData != null}
 		<div
 			id="test-result-title-row"
