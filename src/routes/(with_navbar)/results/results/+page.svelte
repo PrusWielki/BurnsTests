9d58a35990374ef31103fc483c42d929a5f5b884
Patryk <pruswielki@gmail.com>
2023-05-24 10:54:09 +0200

    add test data filtering

--- src/routes/(with_navbar)/results/results/+page.svelte
+++ src/routes/(with_navbar)/results/results/+page.svelte
@@ -2,11 +2,15 @@
 	import { getPagination } from '../../../../hooks/pagination';
 	import { fly } from 'svelte/transition';
 	import type { PageData } from './$types';
+
 	export let data: PageData;
+
 	let { testData, supabase } = data;
 	let page: number = 0;
+	let type: string = 'All';
 	let paginationResult: { from: number; to: number };
 	$: paginationResult = getPagination(page);
+
 	const getTestsData = async (from: number, to: number) => {
 		const { data } = await supabase
 			.from('Tests')
@@ -15,6 +19,9 @@
 			.range(from, to);
 		return data;
 	};
+	type TestDataResponse = Awaited<ReturnType<typeof getTestsData>>;
+	// type MoviesResponseSuccess = TestDataResponse['data'];
+	let filteredTestData: TestDataResponse | undefined;
 	const updateTestsData = (from: number, to: number) => {
 		if (from !== 0) {
 			getTestsData(from, to).then((result) => {
@@ -24,7 +31,12 @@
 			});
 		}
 	};
+	const filterTestData = (type: string, testData: TestDataResponse) => {
+		if (type !== 'All') filteredTestData = testData?.filter((test) => test.type === type && test);
+		else filteredTestData = testData;
+	};
 	$: updateTestsData(paginationResult.from, paginationResult.to);
+	$: filterTestData(type, testData);
 </script>
 
 <div
@@ -32,29 +44,38 @@
 	class="flex h-screen w-screen flex-col items-center justify-center px-4 py-12"
 	in:fly={{ y: -screen.height / 2, duration: 1000 }}
 >
-	{#if testData != null}
+	<div id="filters-container" class="flex-col items-center justify-center py-2">
+		<p class="text-center text-xl text-zinc-300 sm:text-2xl">Filters</p>
+		<select bind:value={type} class="select-bordered select">
+			<option value="All">All</option>
+			<option value="Depression">Depression</option>
+			<option value="Anxiety">Anxiety</option>
+			<option value="Relationships">Relationships</option>
+		</select>
+	</div>
+	{#if filteredTestData != null}
 		<div
 			id="test-result-title-row"
 			class=" mb-12 grid w-full grid-cols-3 items-center justify-center gap-2 text-center md:w-1/2"
 		>
-			<div class="text-xl text-zinc-300 sm:text-2xl">Type</div>
-			<div class="text-xl text-zinc-300 sm:text-2xl">Questions Sum</div>
-			<div class="text-xl text-zinc-300 sm:text-2xl">Date</div>
+			<h1 class="text-xl text-zinc-300 sm:text-2xl">Type</h1>
+			<h1 class="text-xl text-zinc-300 sm:text-2xl">Questions Sum</h1>
+			<h1 class="text-xl text-zinc-300 sm:text-2xl">Date</h1>
 		</div>
 		<div
 			id="test-result-container"
 			class="leanscroll flex max-h-96 w-full flex-col overflow-auto md:w-1/2"
 		>
-			{#each testData as test}
+			{#each filteredTestData as test}
 				<div
 					id="test-result"
 					class="grid w-full grid-cols-3 items-center justify-center gap-2 text-center"
 				>
-					<div class="text-xl text-zinc-300 sm:text-2xl" id="test-name">{test.type}</div>
-					<div class="text-xl text-zinc-300 sm:text-2xl" id="test-sum">{test.questions_sum}</div>
-					<div class="text-xl text-zinc-300 sm:text-2xl" id="test-created">
+					<p class="text-xl text-zinc-300 sm:text-2xl" id="test-name">{test.type}</p>
+					<p class="text-xl text-zinc-300 sm:text-2xl" id="test-sum">{test.questions_sum}</p>
+					<p class="text-xl text-zinc-300 sm:text-2xl" id="test-created">
 						{test.created_at.substring(0, 10)}
-					</div>
+					</p>
 				</div>
 			{/each}
 		</div>
