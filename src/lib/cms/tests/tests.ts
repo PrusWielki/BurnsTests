2ebd50343cc56dd83305537a940918c9df9dadfe
Patryk <pruswielki@gmail.com>
2023-06-06 19:23:36 +0200

    up the img quality :(

--- src/lib/cms/tests/tests.ts
+++ src/lib/cms/tests/tests.ts
@@ -1,7 +1,7 @@
 export const TEST_NAMES = ['Anxiety', 'Depression', 'Relationships'];
 export const TEST_NAMES_LOWER = ['anxiety', 'depression', 'relationships'];
 export const BACKGROUND_NAMES = [
-	'/background/hd/mountains.jpg',
-	'/background/hd/trees.jpg',
-	'/background/hd/animals.jpg'
+	'/background/goodenough/mountains.jpg',
+	'/background/goodenough/trees.jpg',
+	'/background/goodenough/animals.jpg'
 ];
