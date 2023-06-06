26c1420af3d7176c57d515f3fb53cda151e8d802
Patryk <pruswielki@gmail.com>
2023-06-06 13:50:07 +0200

    more refactoring :)

--- src/routes/(with_navbar)/tests/+page.svelte
+++ src/routes/(with_navbar)/tests/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { TEST_NAMES, BACKGROUND_NAMES } from '$lib/cms/tests/tests';
 	import { fly } from 'svelte/transition';
+	import Option from '$lib/components/option/option.svelte';
 </script>
 
 <div
@@ -8,19 +9,10 @@
 	class="grid h-screen w-screen grid-rows-3 overflow-hidden sm:grid-cols-3 sm:grid-rows-none"
 >
 	{#each TEST_NAMES as testName, index}
-		<a
-			id="option-wrapper"
-			style="background-image: url({BACKGROUND_NAMES[index]})"
-			class="group flex h-full w-full cursor-pointer items-center justify-center bg-opacity-80 bg-cover bg-center after:relative after:left-0 after:top-0 after:h-full after:w-full after:bg-black after:bg-opacity-30 after:transition-all after:duration-500 after:content-[''] hover:after:opacity-10 sm:h-screen"
-			href={`/tests/${testName.toLowerCase()}`}
-			in:fly={{ y: -screen.height / 2, duration: 1000 }}
-		>
-			<div
-				id="option"
-				class="absolute z-10 font-sans text-4xl font-bold text-white transition duration-300 group-hover:-translate-y-1 md:text-4xl xl:text-6xl"
-			>
-				{testName}
-			</div>
-		</a>
+		<Option
+			{testName}
+			backgroundName={BACKGROUND_NAMES[index]}
+			navigateTo={`/tests/${testName.toLowerCase()}`}
+		/>
 	{/each}
 </div>
