32c49aa61d96d26ab9c7aff56204257fdf8f87a3
Patryk <pruswielki@gmail.com>
2023-06-08 00:03:02 +0200

    :)

--- src/routes/(with_navbar)/tests/[testName]/+page.svelte
+++ src/routes/(with_navbar)/tests/[testName]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { fade, fly } from 'svelte/transition';
+	import { fade } from 'svelte/transition';
 	import type { PageData } from './$types';
 	import { insertTest } from '../../../../hooks/test_data';
 	import { browser } from '$app/environment';
