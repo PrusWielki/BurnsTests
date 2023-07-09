01b4b7e5b9ab9b85307702a625f8abc4cc43fc58
Patryk <pruswielki@gmail.com>
2023-07-09 15:36:12 +0200

    progress

--- src/routes/(with_navbar)/tests/+page.svelte
+++ src/routes/(with_navbar)/tests/+page.svelte
@@ -1,22 +1,48 @@
 <script lang="ts">
 	import { TEST_NAMES, BACKGROUND_NAMES } from '$lib/cms/tests/tests';
 	import Option from '$lib/components/option/option.svelte';
+	import { fade } from 'svelte/transition';
+
+	let windowWidth: number;
+	let scrollX: number;
+	let scrollY: number;
 </script>
 
-<div
-	id="home-grid"
-	class="carousel-center carousel h-screen w-screen snap-y flex-col items-center justify-center px-48 sm:absolute sm:snap-x sm:flex-row sm:gap-5"
->
-	{#each TEST_NAMES as testName, index}
-		<div class="">
-			<Option
-				{testName}
-				backgroundName={BACKGROUND_NAMES[index]}
-				navigateTo={`/tests/${testName.toLowerCase()}`}
-			/>
-		</div>
-	{/each}
+<svelte:window bind:innerWidth={windowWidth} />
+<div id="home-grid-wrapper" class="flex h-screen w-screen justify-center overflow-hidden">
+	<div
+		id="home-grid"
+		class=" carousel-center carousel h-full w-full snap-y flex-col items-center sm:absolute sm:flex-row sm:gap-5"
+		on:scroll={(event) => {
+			scrollX = event.currentTarget.scrollLeft;
+			scrollY = event.currentTarget.scrollTop;
+		}}
+		on:wheel={(event) => {
+			if (windowWidth > 640 && event.deltaY != 0) {
+				event.currentTarget.scrollTo({
+					left: event.currentTarget.scrollLeft + 1.2 * event.deltaY + event.deltaX,
+					behavior: 'instant'
+				});
+			}
+		}}
+	>
+		{#each TEST_NAMES as testName, index}
+			<div id="option-home-wrapper" class="h-1/3 w-full shrink-0 grow sm:h-5/6 sm:basis-1/3">
+				<Option
+					{testName}
+					backgroundName={BACKGROUND_NAMES[index]}
+					navigateTo={`/tests/${testName.toLowerCase()}`}
+				/>
+			</div>
+		{/each}
+	</div>
+	{#if (windowWidth > 640 && scrollX < 200) || (windowWidth < 640 && scrollY < 100)}
+		<p
+			class="absolute bottom-4 font-sans text-xl font-extrabold capitalize sm:text-3xl"
+			out:fade={{ duration: 200 }}
+			in:fade={{ duration: 200 }}
+		>
+			Scroll for more
+		</p>
+	{/if}
 </div>
-
-<style>
-</style>
