2280ceb3e51d281352aa554555233c66db381f4f
Patryk <78270281+PrusWielki@users.noreply.github.com>
2023-08-10 18:37:18 +0200

    Feature/landingpage (#30)
    * homepage prototype
    
    * onScrolleEnd
    
    * mobile styles plus renaming
    
    * progress, kind of
    
    * homepage with gif
    
    * style fixes and github link
    
    * bigger glow and radial
    
    * add description
    
    * homepage styles
    
    * styles
    
    * bigger gif on desktop
    
    * birghter glow gif
    
    * mobile styles
    
    * text size
    
    * login page progress
    
    * glow changes
    
    * styles
    
    * login and register pages
    
    * fade
    
    * responsive gif size
    
    * media query
    
    * github link open in a new page
    
    * homepage navbar component
    
    * homepage slides
    
    * :)
    
    * with homepage navbar
    
    * :)
    
    * methods refactor
    
    * small desktop style changes
    
    ---------
    
    Co-authored-by: Patryk <pruswielki@gmail.com>

---
+++ src/lib/cms/home/methods.ts
@@ -0,0 +1,35 @@
+const CBT = `<h1>TEAM-CBT</h1>
+<h3>Cognitive-Behavioural Therapy</h3>
+<p class="text-slate-300">
+    For quite some time Cognitive Behavioural Therapy has been the gold standard of psychotherapy.
+    CBT has proven to be effective for the treatment of depression, anxiety and a wide range of
+    emotional difficulties.
+</p>
+
+<p class="text-slate-300">
+    TEAM-CBT is a recent improvement of CBT developed by dr David Burns. With a bit of practice
+    You will learn to perfrom TEAM-CBT on yourself, on your own to finally feel joyful and happy
+    and discover that Your painful feelings actually show what's awesome and beautiful about You.
+</p>`;
+
+const ACT = `<h1>ACT</h1>
+<h3>Acceptance and Commitment Therapy</h3>
+<p class="text-slate-300">
+    ACT, developed by dr. Steven Hayes, is a radically different approach to emotional problems.
+    Instead of arguing with Your painful thoughts You will learn how to relate to them with an
+    attitude of kindness and curiosity so that they no longer have a grip on You. Moreover You will discover Your values, what's truly important to You.
+</p>`;
+
+const MBCT = `<h1>MBCT</h1>
+<h3>Mindfulness-Based Cognitive Therapy</h3>
+<p class="text-slate-300">
+    A very effective approach to make the positive emotional healing last. If You struggle with
+    chronic worrying, self-doubt or lack of self-love, MBCT will be the right fit for You.
+</p>
+<p class="text-slate-300">
+    We will explore how to handle waves of anxiety or any other unwanted feelings in the moment.
+    Overtime You will develop new habbits of thinking that will result in greater joy, confidence
+    and productivity.
+</p>`;
+
+export const METHODS = [CBT, ACT, MBCT];
---
+++ src/lib/components/homepage_methods/homepage_methods.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { METHODS } from '$lib/cms/home/methods';
+</script>
+
+{#each METHODS as method, index}
+	<div
+		id={`section-${index + 1}`}
+		class="flex shrink-0 snap-start items-center justify-center bg-zinc-950 pb-24 pt-6 sm:h-full sm:basis-full sm:py-0"
+	>
+		<div
+			id="cbt-method-main-container"
+			class="z-20 flex h-full w-full items-center justify-center px-4"
+		>
+			<div
+				id="cbt-article-container"
+				class="prose text-center sm:prose-2xl"
+				contenteditable="false"
+				bind:innerHTML={method}
+			/>
+		</div>
+	</div>
+{/each}
