a5828dbe70532ce36962b6b28b1f745529fad833
Patryk <pruswielki@gmail.com>
2023-09-04 00:44:16 +0200

    :)

--- src/routes/(with_navbar)/+layout.svelte
+++ src/routes/(with_navbar)/+layout.svelte
@@ -5,30 +5,28 @@
 
 <div
 	id="navbar-container"
-	class="fixed left-1/2 z-30 flex w-full -translate-x-1/2 flex-row justify-center gap-1 px-1 py-1"
+	class="fixed left-1/2 z-30 flex w-full -translate-x-1/2 flex-row justify-center gap-1 bg-black bg-opacity-20 px-1 py-1 backdrop-blur-sm"
 	in:fly={{ y: -screen.height / 2, duration: 500 }}
 >
-	<!-- {#if browser && history.length > 2}
-		<a
-			id="navbar-tests-button"
-			in:fly={{ y: -screen.height / 2, duration: 1000 }}
-			class="btn z-30 bg-transparent text-slate-300 transition duration-75 hover:-translate-y-0.5 hover:shadow-lg sm:text-2xl"
-			on:click={() => history.back()}
-			on:keydown={() => history.back()}>Go Back</a
-		>
-	{/if} -->
 	<a
 		href="/tests"
 		id="navbar-tests-button"
 		in:fly={{ y: -screen.height / 2, duration: 1000 }}
-		class="btn z-30 w-28 rounded-sm border-b-2 border-l-0 border-r-0 border-t-0 bg-transparent text-xl font-bold text-slate-100 transition duration-75 hover:-translate-y-0.5 hover:shadow-lg sm:text-2xl"
+		class="btn z-40 w-28 rounded-sm border-b-2 border-l-0 border-r-0 border-t-0 bg-transparent text-xl font-bold text-slate-100 transition duration-75 hover:-translate-y-0.5 hover:shadow-lg sm:text-2xl"
 		>Tests</a
 	>
+	<!-- <a
+		href="/methods"
+		id="navbar-tests-button"
+		in:fly={{ y: -screen.height / 2, duration: 1000 }}
+		class="btn z-40 w-28 rounded-sm border-b-2 border-l-0 border-r-0 border-t-0 bg-transparent text-xl font-bold text-slate-100 transition duration-75 hover:-translate-y-0.5 hover:shadow-lg sm:text-2xl"
+		>Methods</a
+	> -->
 	<a
 		href="/results"
 		id="navbar-results-button"
 		in:fly={{ y: -screen.height / 2, duration: 1000 }}
-		class="btn z-30 w-28 rounded-sm border-b-2 border-l-0 border-r-0 border-t-0 bg-transparent text-xl font-bold text-slate-100 transition duration-75 hover:-translate-y-0.5 hover:shadow-lg sm:text-2xl"
+		class="btn z-40 w-28 rounded-sm border-b-2 border-l-0 border-r-0 border-t-0 bg-transparent text-xl font-bold text-slate-100 transition duration-75 hover:-translate-y-0.5 hover:shadow-lg sm:text-2xl"
 		>Results</a
 	>
 	<a
@@ -36,7 +34,7 @@
 		id="navbar-logout-button"
 		in:fly={{ y: -screen.height / 2, duration: 1000 }}
 		type="submit"
-		class="btn z-30 w-28 rounded-sm border-b-2 border-l-0 border-r-0 border-t-0 bg-transparent text-xl font-bold text-slate-100 transition duration-75 hover:-translate-y-0.5 hover:shadow-lg sm:fixed sm:right-2 sm:text-2xl"
+		class="btn z-40 w-28 rounded-sm border-b-2 border-l-0 border-r-0 border-t-0 bg-transparent text-xl font-bold text-slate-100 transition duration-75 hover:-translate-y-0.5 hover:shadow-lg sm:fixed sm:right-2 sm:text-2xl"
 		>logout</a
 	>
 </div>
--- src/routes/(with_navbar)/results/results/+page.server.ts
+++ src/routes/(with_navbar)/results/results/+page.server.ts
@@ -1,17 +1,11 @@
-import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ locals: { supabase, getSession } }) => {
-	const session = await getSession();
-
-	if (!session) {
-		throw redirect(302, '/');
-	}
+export const load: PageServerLoad = async ({ locals: { supabase } }) => {
 	const { data } = await supabase
 		.from('Tests')
 		.select()
 		.order('created_at', { ascending: false })
 		.range(0, 10);
 
-	return { session: session, testData: data };
+	return { testData: data };
 };
