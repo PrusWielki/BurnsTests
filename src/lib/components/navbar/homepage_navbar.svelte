9d0e012d34b68197f382727496264dbcba8a5c37
Patryk <pruswielki@gmail.com>
2023-10-21 14:01:22 +0200

    navbar container

--- src/lib/components/navbar/homepage_navbar.svelte
+++ src/lib/components/navbar/homepage_navbar.svelte
@@ -3,20 +3,26 @@
 	import { TITLE } from '$lib/cms/home/home';
 </script>
 
-<a
-	id="title-container"
-	href="/"
-	class="absolute left-4 top-4 z-10 cursor-pointer font-sans font-semibold decoration-wavy hover:text-accent hover:underline sm:left-12"
->
-	{TITLE}
-</a>
-<ThemePicker class="absolute top-4 sm:right-40 right-24" />
-<a
-	target="_blank"
-	rel="noopener noreferrer"
-	id="github-link"
-	class="absolute right-4 top-4 z-10 cursor-pointer font-extrabold decoration-wavy hover:text-accent hover:underline sm:right-12"
-	href="https://github.com/PrusWielki/BurnsTests"
->
-	GitHub
-</a>
+<div class="w-full fixed z-10 top-0">
+	<div class="container mx-auto px-4 py-4 flex flex-row justify-between">
+		<a
+			id="title-container"
+			href="/"
+			class=" cursor-pointer font-sans font-semibold decoration-wavy hover:text-accent hover:underline sm:left-12"
+		>
+			{TITLE}
+		</a>
+		<div class="flex flex-row gap-8">
+			<ThemePicker class="" />
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				id="github-link"
+				class=" z-10 cursor-pointer font-extrabold decoration-wavy hover:text-accent hover:underline sm:right-12"
+				href="https://github.com/PrusWielki/BurnsTests"
+			>
+				GitHub
+			</a>
+		</div>
+	</div>
+</div>
