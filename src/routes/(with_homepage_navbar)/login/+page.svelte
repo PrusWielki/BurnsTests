3793db42d13f5a701a1341390aef9ec25b49c67e
Patryk <pruswielki@gmail.com>
2023-11-25 18:52:46 +0100

    update login, register pages

--- src/routes/(with_homepage_navbar)/login/+page.svelte
+++ src/routes/(with_homepage_navbar)/login/+page.svelte
@@ -8,52 +8,57 @@
 	let email: string;
 </script>
 
-<div
-	id="login_main_container"
-	class="dynamic-full-screen flex w-full flex-col items-center justify-center gap-8 bg-base-100 px-6 sm:gap-12"
->
-	<h1 class="text-center text-2xl font-bold sm:text-3xl">Welcome Back</h1>
-	<form
-		id="login-form"
-		action="?/login"
-		method="POST"
-		class="flex w-full flex-col items-center justify-center gap-4 sm:gap-8"
-		in:fade={{ duration: 200 }}
-		use:enhance
-	>
-		<input
-			type="text"
-			name="email"
-			class="w-3/4 input input-bordered sm:w-1/4"
-			bind:value={email}
-			placeholder="Email"
-		/>
-
-		<input
-			type="password"
-			name="password"
-			class="w-3/4 input input-bordered sm:w-1/4"
-			placeholder="Password"
-		/>
-
-		<button id="submit-button" type="submit" class="w-3/4 sm:w-1/4 btn btn-neutral"> Login</button>
-		<button
-			on:click={async () => {
-				if (!email) alert('Please enter email');
-				else {
-					await supabase.auth.resetPasswordForEmail(email, {
-						redirectTo: 'https://burns-tests.vercel.app/updatepassword'
-					});
-					alert('Check your email inbox');
-				}
-			}}
-			class="text-lg italic decoration-wavy transition duration-75 hover:text-accent hover:underline"
-			>Forgot Password?</button
+<div id="login_main_wrapper" class="my-auto w-full bg-base-100 px-6 py-16 sm:gap-12">
+	<div class="flex flex-col w-full items-center gap-6 max-w-xl mx-auto">
+		<h1 class="text-center text-2xl font-bold sm:text-3xl">Welcome Back</h1>
+		<form
+			id="login-form"
+			action="?/login"
+			method="POST"
+			class="flex w-full flex-col items-center justify-center gap-4 sm:gap-8"
+			in:fade={{ duration: 200 }}
+			use:enhance
 		>
-	</form>
-	{#if form?.error}
-		<div class="absolute bottom-20 text-2xl font-semibold text-accent sm:bottom-32 sm:text-3xl">
-			{form?.error}
-		</div>
-	{/if}
+			<input
+				type="text"
+				name="email"
+				class="w-3/4 input input-bordered md:w-1/2 lg:w-2/3"
+				bind:value={email}
+				placeholder="Email"
+			/>
+
+			<input
+				type="password"
+				name="password"
+				class="w-3/4 input input-bordered md:w-1/2 lg:w-2/3"
+				placeholder="Password"
+			/>
+
+			<button
+				id="submit-button"
+				type="submit"
+				class="w-3/4 md:w-1/2 lg:w-2/3 btn btn-primary text-xl"
+			>
+				Login</button
+			>
+			<button
+				on:click={async () => {
+					if (!email) alert('Please enter email');
+					else {
+						await supabase.auth.resetPasswordForEmail(email, {
+							redirectTo: 'https://burns-tests.vercel.app/updatepassword'
+						});
+						alert('Check your email inbox');
+					}
+				}}
+				class="text-lg italic decoration-wavy transition duration-75 hover:text-accent hover:underline"
+				>Forgot Password?</button
+			>
+		</form>
+		{#if form?.error}
+			<div class="absolute bottom-20 text-2xl font-semibold text-accent sm:bottom-32 sm:text-3xl">
+				{form?.error}
+			</div>
+		{/if}
+	</div>
 </div>
--- src/routes/(with_homepage_navbar)/register/+page.svelte
+++ src/routes/(with_homepage_navbar)/register/+page.svelte
@@ -4,7 +4,7 @@
 	export let form;
 </script>
 
-<div id="register_main_wrapper" class=" w-full bg-base-100 px-6 py-40 sm:gap-12">
+<div id="register_main_wrapper" class="my-auto w-full bg-base-100 px-6 py-16 sm:gap-12">
 	<div class="flex flex-col w-full items-center gap-6 max-w-xl mx-auto">
 		<h1 class="text-center text-2xl font-bold sm:text-3xl">Glad to see You</h1>
 		<h2 class="text-center font-semibold max-w-md [text-wrap:balance]">
