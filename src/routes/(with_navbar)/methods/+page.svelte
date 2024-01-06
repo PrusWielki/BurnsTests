dd51a0d03106806c6bc1599f7894256d383b523b
PrusWielki <prusakpat@gmail.com>
2024-01-06 19:27:32 +0100

    :)

--- src/routes/(with_navbar)/methods/+page.svelte
+++ src/routes/(with_navbar)/methods/+page.svelte
@@ -1,3 +1,3 @@
 <section id="test-wrapper" class="w-full h-full bg-base-100">
-	<div class="container px-6 py-20 mx-auto flex flex-col items-center w-full">Methods go here</div>
+	<div class="container px-6 py-20 mx-auto flex flex-col items-center w-full">Methods go here:</div>
 </section>
