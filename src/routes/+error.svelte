dc4cb4cac7fda6ed943ad30facc0da7ff2fc4b1b
PrusWielki <prusakpat@gmail.com>
2024-01-25 20:54:30 +0100

    update error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,9 +5,12 @@
 
 <section class="w-full flex items-center dynamic-full-screen bg-base-100">
 	<div class="container px-6 py-20 mx-auto flex flex-col items-center w-full gap-4">
-		<h1 class="text-xl sm:text-4xl text-error font-extrabold">
-			ERROR: {$page.error && $page.error.message}
+		<h1 class="text-xl sm:text-4xl text-neutral-content font-extrabold">
+			Ups! Something went wrong.
 		</h1>
+		<h2 class="text-xl sm:text-4xl text-error font-extrabold">
+			{$page.error && $page.error.message}
+		</h2>
 		<button
 			class="btn btn-primary text-lg sm:text-2xl"
 			on:click={() => {
