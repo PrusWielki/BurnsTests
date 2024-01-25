70905526ab1d8b798e5294484e3ca126c96c0a22
PrusWielki <prusakpat@gmail.com>
2024-01-25 20:52:23 +0100

    add info to error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,10 +1,13 @@
 <script>
 	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 </script>
 
 <section class="w-full flex items-center dynamic-full-screen bg-base-100">
 	<div class="container px-6 py-20 mx-auto flex flex-col items-center w-full gap-4">
-		<h1 class="text-4xl sm:text-6xl text-error font-extrabold">ERROR</h1>
+		<h1 class="text-xl sm:text-4xl text-error font-extrabold">
+			ERROR: {$page.error && $page.error.message}
+		</h1>
 		<button
 			class="btn btn-primary text-lg sm:text-2xl"
 			on:click={() => {
