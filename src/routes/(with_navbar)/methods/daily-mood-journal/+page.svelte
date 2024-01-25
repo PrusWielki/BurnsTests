33faa668f3cb8154a32a2408e8e8cb5d9ee8e8a0
PrusWielki <prusakpat@gmail.com>
2024-01-25 20:33:24 +0100

    add error page

---
+++ src/routes/(with_navbar)/methods/daily-mood-journal/+page.svelte
@@ -0,0 +1,3 @@
+<section class="w-full h-full bg-base-100">
+	<div class="container px-6 py-20 mx-auto flex flex-col items-center w-full"></div>
+</section>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { goto } from '$app/navigation';
+</script>
+
+<section class="w-full flex items-center dynamic-full-screen bg-base-100">
+	<div class="container px-6 py-20 mx-auto flex flex-col items-center w-full gap-4">
+		<h1 class="text-4xl sm:text-6xl text-error font-extrabold">ERROR</h1>
+		<button
+			class="btn btn-primary text-lg sm:text-2xl"
+			on:click={() => {
+				goto('/');
+			}}>Go back to homepage</button
+		>
+	</div>
+</section>
